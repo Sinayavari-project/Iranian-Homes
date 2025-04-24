1f98cd87d61ab5d2c132964d5b7af9f759cc8bdb
Your Name <your.email@example.com>
2025-04-24 17:23:39 +0400

    Remove i18n config and fix TypeScript types

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    i18n: {
-        locales: ['en', 'fa'],
-        defaultLocale: 'en',
-        localeDetection: true,
-    },
 }
 
 module.exports = nextConfig
